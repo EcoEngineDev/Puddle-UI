8a08fbf6fe00698667c33586fd24f22486d18fe5
Charlie <charlierusso01@gmail.com>
2025-07-08 09:52:27 -0400

    button fixes

--- navbar.py
+++ navbar.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton
 from PyQt5.QtCore import Qt
-from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtGui import QIcon
 import os
 
 class navWidget(QWidget):
@@ -13,34 +13,48 @@ class navWidget(QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(16)
 
-        # Example: Add a home icon SVG
+        # Home icon button
         home_path = os.path.join("Media", "Icons", "home-hashtag-svgrepo-com.svg")
         if os.path.exists(home_path):
-            home_icon = QPushButton(QSvgWidget(home_path))
+            home_icon = QPushButton()
+            home_icon.setIcon(QIcon(home_path))
+            home_icon.setIconSize(home_icon.size() * 0.2)  # Make icon 60% of button size
             home_icon.setFixedSize(128, 128)
+            home_icon.clicked.connect(lambda: self.button_clicked("Home"))
             layout.addWidget(home_icon)
+
+        # Music icon button
         music_path = os.path.join("Media", "Icons", "music-svgrepo-com.svg")
         if os.path.exists(music_path):
-            music_icon = QPushButton(QSvgWidget(music_path))
+            music_icon = QPushButton()
+            music_icon.setIcon(QIcon(music_path))
+            music_icon.setIconSize(music_icon.size() * 0.2)  # Make icon 60% of button size
             music_icon.setFixedSize(128, 128)
+            music_icon.clicked.connect(lambda: self.button_clicked("Music"))
             layout.addWidget(music_icon)
+
+        # Maps icon button
         maps_path = os.path.join("Media", "Icons", "route-square-svgrepo-com.svg")
         if os.path.exists(maps_path):
-            maps_icon = QPushButton(QSvgWidget(maps_path))
+            maps_icon = QPushButton()
+            maps_icon.setIcon(QIcon(maps_path))
+            maps_icon.setIconSize(maps_icon.size() * 0.2)  # Make icon 60% of button size
             maps_icon.setFixedSize(128, 128)
+            maps_icon.clicked.connect(lambda: self.button_clicked("Maps"))
             layout.addWidget(maps_icon)
+
+        # Games icon button
         games_path = os.path.join("Media", "Icons", "gameboy-svgrepo-com.svg")
         if os.path.exists(games_path):
-            games_icon = QPushButton(QSvgWidget(games_path))
+            games_icon = QPushButton()
+            games_icon.setIcon(QIcon(games_path))
+            games_icon.setIconSize(games_icon.size() * 0.2)  # Make icon 60% of button size
             games_icon.setFixedSize(128, 128)
+            games_icon.clicked.connect(lambda: self.button_clicked("Games"))
             layout.addWidget(games_icon)
-
-        # Add more icons here as needed
-        # e.g. settings_icon = QSvgWidget(...)
-        # layout.addWidget(settings_icon)
 
         layout.setAlignment(Qt.AlignCenter)
         self.setLayout(layout)
 
-        def button_clicked(self):
-            print("Button clicked!")
+    def button_clicked(self, button_name):
+        print(f"{button_name} button clicked!")
